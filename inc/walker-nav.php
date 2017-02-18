a37d01cc683e7f70c4b2255ddcb8f5c65de76f2b
shubham9411 <shubham9411@gmail.com>
2017-02-18 16:40:12 +0530

    Spaces in documentation

--- inc/walker-nav.php
+++ inc/walker-nav.php
@@ -17,12 +17,13 @@ if ( ! class_exists( 'Walker_Nav_top' ) ) {
 	class Walker_Nav_top extends Walker_Nav_Menu {
 		/**
 		 * Ends the list of after the elements are added.
+		 * 
 		 * @see Walker::start_lvl()
 		 * @since 3.0.0
 		 *
-		 * @param string    $output Passed by reference. Used to append additional content.
-		 * @param int    $depth Depth of page. Used for padding.
-		 * @param array    $args An array of additional arguments.
+		 * @param string output Passed by reference. Used to append additional content.
+		 * @param int  $depth Depth of page. Used for padding.
+		 * @param array $args An array of additional arguments.
 		 */
 		public function start_lvl( &$output, $depth = 0, $args = array() ) {
 			$indent = str_repeat( "\t", $depth );
@@ -30,13 +31,15 @@ if ( ! class_exists( 'Walker_Nav_top' ) ) {
 		}
 
 		/**
+		 * Manipulate the elements eg. link, span, li
+		 * 
 		 * @see Walker::start_el()
 		 * @since 3.0.0
 		 *
-		 * @param string    $output Passed by reference. Used to append additional content.
-		 * @param object    $item Menu item data object.
+		 * @param string $output Passed by reference. Used to append additional content.
+		 * @param object $item Menu item data object.
 		 * @param int    $depth Depth of menu item. Used for padding.
-		 * @param object    $args The data object.
+		 * @param object $args The data object.
 		 * @param int    $id Menu item ID.
 		 */
 		public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
@@ -51,13 +54,17 @@ if ( ! class_exists( 'Walker_Nav_top' ) ) {
 			 */
 			if ( 0 == strcasecmp( $item->attr_title, 'divider' ) && 1 === $depth ) {
 				$output .= $indent . '<li role="presentation" class="divider">';
-			} elseif ( 0 == strcasecmp( $item->title, 'divider') && 1 === $depth ) {
+			} 
+			elseif ( 0 == strcasecmp( $item->title, 'divider') && 1 === $depth ) {
 				$output .= $indent . '<li role="presentation" class="divider">';
-			} elseif ( 0 == strcasecmp( $item->attr_title, 'dropdown-header') && 1 === $depth ) {
+			} 
+			elseif ( 0 == strcasecmp( $item->attr_title, 'dropdown-header') && 1 === $depth ) {
 				$output .= $indent . '<li role="presentation" class="dropdown-header">' . esc_attr( $item->title );
-			} elseif ( 0 == strcasecmp($item->attr_title, 'disabled' ) ) {
+			} 
+			elseif ( 0 == strcasecmp($item->attr_title, 'disabled' ) ) {
 				$output .= $indent . '<li role="presentation" class="disabled"><a href="#">' . esc_attr( $item->title ) . '</a>';
-			} else {
+			} 
+			else {
 				$class_names = $value = '';
 				$classes = empty( $item->classes ) ? array() : (array) $item->classes;
 				$classes[] = 'menu-item-' . $item->ID;
@@ -104,9 +111,9 @@ if ( ! class_exists( 'Walker_Nav_top' ) ) {
 				 */
 				if ( ! empty( $item->attr_title ) ) {
 					$item_output .= '<a' . $attributes . '><span class="glyphicon ' . esc_attr( $item->attr_title ) . '"></span>&nbsp;';
-				}
+				} 
 				else {
-					$item_output .= '<a' . $attributes .'>';
+					$item_output .= '<a' . $attributes . '>';
 				}
 				$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
 				$item_output .= ( $args->has_children && 0 === $depth ) ? ' <span class="caret"></span></a>' : '</a>';
@@ -127,13 +134,13 @@ if ( ! class_exists( 'Walker_Nav_top' ) ) {
 		 * @see Walker::start_el()
 		 * @since 2.5.0
 		 *
-		 * @param object    $element Data object.
-		 * @param array    $children_elements List of elements to continue traversing.
-		 * @param int    $max_depth Max depth to traverse.
-		 * @param int    $depth Depth of current element.
-		 * @param array    $args The array object.
-		 * @param string    $output Passed by reference. Used to append additional content.
-		 * @return null    Null on failure with no changes to parameters.
+		 * @param object $element Data object.
+		 * @param array $children_elements List of elements to continue traversing.
+		 * @param int  $max_depth Max depth to traverse.
+		 * @param int  $depth Depth of current element.
+		 * @param array $args The array object.
+		 * @param string $output Passed by reference. Used to append additional content.
+		 * @return null Null on failure with no changes to parameters.
 		 */
 		public function display_element( $element, &$children_elements, $max_depth, $depth, $args, &$output ) {
 			if ( ! $element ) {
