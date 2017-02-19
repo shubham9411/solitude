be3a26040bd6c59a92281db0440e37579f3177d8
shubham9411 <shubham9411@gmail.com>
2017-02-19 12:05:08 +0530

    Bug fixing for js and escaping

--- inc/custom-styles.php
+++ inc/custom-styles.php
@@ -38,16 +38,15 @@ function solitude_custom_css() {
 }
 #site-header-nav{
 	border-top: ' . $border_size . 'px solid ' . $header_color . ';
-}'; 
+}';
 	/**
 	 * Filters Solitude custom colors CSS.
 	 *
 	 * @since Solitude 1.0
 	 *
-	 * @param $border_size int The user's selected color hue.
-	 * @param $css         string Base theme colors CSS.
+	 * @param $css string Base theme colors CSS.
 	 */
-	return apply_filters( 'solitude_custom_css' , $border_size , $css );
+	return apply_filters( 'solitude_custom_css' , $css );
 }
 
 /**
@@ -56,7 +55,7 @@ function solitude_custom_css() {
 function solitude_colors_css_wrap() {
 ?>
 	<style type="text/css">
-		<?php echo solitude_custom_css(); ?>
+		<?php echo esc_html( solitude_custom_css() ); ?>
 	</style>
 <?php }
 add_action( 'wp_head', 'solitude_colors_css_wrap' );
